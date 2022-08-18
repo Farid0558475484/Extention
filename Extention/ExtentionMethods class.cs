ddee7704ce5c880b11966ba36e367d9f6205e0e0
Farid0558475484 <faridmh@code.edu.az>
2022-08-18 18:38:36 +0400

    Extention

---
+++ Extention/ExtentionMethods class.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Extention
+{
+    public static class ExtentionMethods_class
+    {
+        static public bool IsOdd(this int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+
+        static public bool IsEven(this int num1)
+        {
+            if (num1 % 2 == 0)
+
+            {
+                return true;
+            }
+            return false;
+        }
+        static public bool IsContainsDigit(this string character)
+        {
+
+            for (int i = 0; i < character.Length; i++)
+            {
+                if (char.IsDigit(character[i]))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+
+        static public string ToCapitalize(this string temp)
+        {
+
+            StringBuilder sb1 = new StringBuilder();
+
+            sb1.Append(char.ToUpper(temp[0]));
+            sb1.Append(temp.Substring(1).ToLower());
+            return sb1.ToString();
+            
+        }
+    }
+}
+
---
+++ Extention/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extention
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string word = "salam";
+            Console.WriteLine(word.ToCapitalize());
+        }
+    }
+}
